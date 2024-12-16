92c87f1377ac56a326a08e162d5cd3daa589094c
mitchy-dev <mitchy.dev@gmail.com>
2024-12-17 08:00:17 +0900

    TodoItem分割テスト：完了状態の切替の関数をpropsで受け取り

--- js-version/src/app/todo/todo-list/todo-item.test.js
+++ js-version/src/app/todo/todo-list/todo-item.test.js
@@ -1,11 +1,27 @@
 import { render, screen } from "@testing-library/react";
-import {TodoItem} from "@/app/todo/todo-list/todo-item";
+import {TodoItem} from "./todo-item";
+import userEvent from "@testing-library/user-event";
 
 describe("タスクコンポーネント", () => {
+  const mockProps = { //型に応じたモックデータ
+    id: 1,
+    text: 'todo1',
+    isDone: false,
+    onToggleDone: jest.fn(),
+  };
   beforeEach(() => {
-    render(<TodoItem />);
+    render(<TodoItem {...mockProps} />); //型に応じたモックデータを渡す
   });
   test("初期表示", () => {
-    expect(screen.getByText('TodoItem')).toBeInTheDocument();
+    expect(screen.getByText(mockProps.text)).toBeInTheDocument(); //呼出チェック
   });
+  test("未完了のタスクをクリック：完了状態になる", async () => {
+    const checkIcon = screen.getByLabelText('タスクの完了状態');
+    const user = userEvent.setup();
+    await user.click(checkIcon);
+    expect(mockProps.onToggleDone).toHaveBeenCalledWith(mockProps.id, mockProps.isDone); //呼出チェック、h1とは異なりユーザーイベントが事前に必要
+  });
+//   タスクをクリック：編集モードになる
+//   タスクを編集：入力値が反映される
+//   ゴミ箱アイコンクリック：当該要素が消える
 });
