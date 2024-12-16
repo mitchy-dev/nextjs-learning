71094dfbdaee77e4bba7eb47d479dfa159dd7d00
mitchy-dev <mitchy.dev@gmail.com>
2024-12-17 08:00:00 +0900

    TodoItem分割：完了状態の切替の関数をpropsで受け取り

--- js-version/src/app/todo/todo-list/todo-item.js
+++ js-version/src/app/todo/todo-list/todo-item.js
@@ -1,5 +1,18 @@
-export function TodoItem () {
+import {PropTypes} from "prop-types";
+
+TodoItem.propTypes = { //propsの型定義、TS化の際に参考になる
+  id: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  isDone: PropTypes.bool.isRequired,
+  onToggleDone: PropTypes.func.isRequired,
+}
+
+export function TodoItem ({id, text, isDone, onToggleDone}) { //型に応じたpropsを渡す
   return (
-    <h1>TodoItem</h1>
+      <div>
+        <h1>{text}</h1> //呼出
+        <i aria-label="タスクの完了状態"
+            onClick={() => onToggleDone(id, isDone)}></i>
+      </div>
   );
 }
