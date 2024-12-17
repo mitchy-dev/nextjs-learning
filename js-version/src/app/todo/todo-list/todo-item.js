fa80f18e686b35720d6b518401ed195d673d9fec
mitchy-dev <mitchy.dev@gmail.com>
2024-12-17 12:40:35 +0900

    feat: タスクテキストのクリックイベントを実装

--- js-version/src/app/todo/todo-list/todo-item.js
+++ js-version/src/app/todo/todo-list/todo-item.js
@@ -4,13 +4,17 @@ TodoItem.propTypes = { //propsの型定義、TS化の際に参考になる
   id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   isDone: PropTypes.bool.isRequired,
+  isEdit: PropTypes.bool.isRequired,
   onToggleDone: PropTypes.func.isRequired,
+  onShowEdit: PropTypes.func.isRequired,
 }
 
-export function TodoItem ({id, text, isDone, onToggleDone}) { //型に応じたpropsを渡す
+export function TodoItem ({id, text, isDone, isEdit, onToggleDone, onShowEdit}) { //型に応じたpropsを渡す
   return (
       <div>
-        <h1>{text}</h1> //呼出
+        {/*呼出*/}
+        <span aria-label="タスクのテキスト"
+          onClick={() => onShowEdit(id, isEdit)}>{text}</span>
         <i aria-label="タスクの完了状態"
             onClick={() => onToggleDone(id, isDone)}></i>
       </div>
--- js-version/src/app/todo/todo-list/todo-item.test.js
+++ js-version/src/app/todo/todo-list/todo-item.test.js
@@ -6,8 +6,10 @@ describe("タスクコンポーネント", () => {
   const mockProps = { //型に応じたモックデータ
     id: 1,
     text: 'todo1',
+    isEdit: false,
     isDone: false,
     onToggleDone: jest.fn(),
+    onShowEdit: jest.fn(),
   };
   beforeEach(() => {
     render(<TodoItem {...mockProps} />); //型に応じたモックデータを渡す
@@ -22,6 +24,12 @@ describe("タスクコンポーネント", () => {
     expect(mockProps.onToggleDone).toHaveBeenCalledWith(mockProps.id, mockProps.isDone); //呼出チェック、h1とは異なりユーザーイベントが事前に必要
   });
 //   タスクをクリック：編集モードになる
+  test("タスクをクリック：編集モードになる", async () => {
+   const displayElement = screen.getByLabelText('タスクのテキスト');
+   const user = userEvent.setup();
+   await user.click(displayElement);
+   expect(mockProps.onShowEdit).toHaveBeenCalledWith(mockProps.id, mockProps.isEdit); //呼出チェック、h1とは異なりユーザーイベントが事前に必要
+  });
 //   タスクを編集：入力値が反映される
 //   ゴミ箱アイコンクリック：当該要素が消える
 });
