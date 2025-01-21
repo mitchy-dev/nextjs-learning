9a83964571ed1b6abf05be50fc2a28fb31c65df6
mitchy-dev <mitchy.dev@gmail.com>
2025-01-22 08:18:01 +0900

    test: lucideをモックに置き換え

--- js-version/jest.config.js
+++ js-version/jest.config.js
@@ -7,6 +7,10 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^lucide-react$': '<rootDir>/src/tests/mocks/lucide-react.js',
+    '^lucide-react/dist/(.*)$': '<rootDir>/src/tests/mocks/lucide-react.js'
+  }
 }
 
 module.exports = createJestConfig(customJestConfig)
