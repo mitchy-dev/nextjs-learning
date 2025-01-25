2f8a37a3fe45840b5643bed604785690ce3acc60
mitchy-dev <mitchy.dev@gmail.com>
2025-01-26 08:42:30 +0900

    feat: 国際化ライブラリの導入

--- js-version/src/app/todo/_components/TodoSearch/index.js
+++ js-version/src/app/todo/_components/TodoSearch/index.js
@@ -1,16 +1,18 @@
-import {todoSearchPropTypes} from "../types/props";
 import {Search} from "lucide-react";
+import {useTranslations} from 'next-intl';
+
+import {todoSearchPropTypes} from "../types/props";
 export function TodoSearch ({ defaultValue, onChange }) {
-  
+  const t = useTranslations('TodoSearch');
   return (
       <div className="searchBox">
         <Search className="searchBox__icon" aria-hidden="true" />
-        <label htmlFor="todo-search" className="visually-hidden">タスクを検索</label>
+        <label htmlFor="todo-search" className="visually-hidden">{t('label')}</label>
         <input type="search"
                id="todo-search"
                className="searchBox__input js-search"
                defaultValue={defaultValue}
-               placeholder="something keyword"
+               placeholder={t('placeholder')}
                onChange={(e) => onChange(e.target.value)}
         />
       </div>
--- js-version/src/app/todo/_components/TodoSearch/index.test.js
+++ js-version/src/app/todo/_components/TodoSearch/index.test.js
@@ -2,14 +2,22 @@ import { render, screen } from "@testing-library/react";
 import {TodoSearch} from "@/app/todo/_components/TodoSearch/index";
 import userEvent from "@testing-library/user-event";
 
+import {NextIntlClientProvider} from 'next-intl';
+import messages from '@messages/ja.json'
+
 describe("タスク検索コンポーネント", () => {
   const mockDefaultValue = "";
   const mockOnChange = jest.fn();
-  test("初期表示", () => {
-     render(<TodoSearch defaultValue={mockDefaultValue} onChange={mockOnChange} />);
+  test("初期表示", async () => {
+     render(
+         // todo プロバイダーの記述を共通化する
+         <NextIntlClientProvider messages={messages} locale="ja">
+          <TodoSearch defaultValue={mockDefaultValue} onChange={mockOnChange} />
+         </NextIntlClientProvider>
+     );
      const textbox = screen.getByRole('searchbox');
-     expect(screen.getByLabelText('タスクを検索')).toBeInTheDocument();
-     expect(textbox).toHaveAttribute('placeholder', 'something keyword');
+     expect(screen.getByLabelText(messages.TodoSearch.label)).toBeInTheDocument();
+     expect(textbox).toHaveAttribute('placeholder', messages.TodoSearch.placeholder);
      expect(textbox).toHaveValue('');
   });
   test("onChangeで関数呼び出し", async () => {
