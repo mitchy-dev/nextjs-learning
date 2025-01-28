07ed7fe5536ed51b045e6b1eca2e52e6d6cd0a65
mitchy-dev <mitchy.dev@gmail.com>
2025-01-28 12:45:56 +0900

    refactor: 文言の管理を言語ファイルに移行（タスク）

--- js-version/src/app/todo/_components/TodoItem/index.js
+++ js-version/src/app/todo/_components/TodoItem/index.js
@@ -1,15 +1,19 @@
 import classNames from 'classnames';
+import {useTranslations} from 'next-intl';
+
 import {todoItemPropTypes} from "../types/props";
-import PropTypes from "prop-types";
 
 export function TodoItem ({task, handlers}) { //型に応じたpropsを渡す
   const {id, text, isDone, isEdit} = task;
   const {onToggleDone, onShowEdit, onChangeText, onRemoveTask, onConfirmEdit,} = handlers;
+  
+  const t = useTranslations('TodoItem');
   const classNameLi = classNames(
    'list__item', {
         'is-done': isDone
       });
   const classNameIcon = classNames(
+    //   todo lucideに変更
     'fa', 'icon-check', {
          'fa-square-o': !isDone,
          'fa-check-square': isDone,
@@ -17,22 +21,24 @@ export function TodoItem ({task, handlers}) { //型に応じたpropsを渡す
   return (
       <li key={id} className={classNameLi}>
        {/*ToDo buttonタグの追加:iタグでbutton代用しているため*/}
+        {/* todo 文言をja.jsonに移行 */}
         <i className={classNameIcon}
-           aria-label="タスクの完了状態"
+           aria-label={t('completeLabel')}
            onClick={() => onToggleDone(id, isDone)} />
         {isEdit ?
             <input
                 type="text"
                 defaultValue={text}
-                aria-label="タスク編集"
+                aria-label={t('editLabel')}
                 onChange={(e) => onChangeText(id, e.target.value)}
                 onKeyUp={(e) => onConfirmEdit(e, id)}
             /> :
-            <span aria-label="タスクのテキスト"
+            <span aria-label={t('textLabel')}
                   onClick={() => onShowEdit(id)}>{text}</span>
           }
+        {/*  todo lucideに変更に変更 */}
         <i className="fa fa-trash icon-trash"
-           aria-label={`タスク削除${id}`}
+           aria-label={t('deleteLabel', {id})}
            onClick={() => onRemoveTask(id)} />
       </li>
   
@@ -40,3 +46,7 @@ export function TodoItem ({task, handlers}) { //型に応じたpropsを渡す
 }
 
 TodoItem.propTypes = todoItemPropTypes;
+
+// リファクタリングの順番
+// テストへの影響が小さいものから着手
+// 文言　＞　アイコン　＞　button実装
--- js-version/src/app/todo/_components/TodoItem/index.test.js
+++ js-version/src/app/todo/_components/TodoItem/index.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import PropTypes from "prop-types";
 
+import messages from '@messages/ja.json'
+import { render } from "@/test/test-utils"
 import {createMockTodoItemProps} from "@/tests/factories/todo";
 import {TodoItem} from "./index";
 import {taskPropType, taskShape, todoHandlersPropTypes} from "@/app/todo/_components/types/props";
@@ -31,13 +33,13 @@ describe("タスクコンポーネント", () => {
     expect(screen.getByText(createMockTodoItemProps().task.text)).toBeInTheDocument(); //呼出チェック
   });
   test("未完了のタスクをクリック：完了状態になる", async () => {
-    const checkIcon = screen.getByLabelText('タスクの完了状態');
+    const checkIcon = screen.getByLabelText(messages.TodoItem.completeLabel);
     const user = userEvent.setup();
     await user.click(checkIcon);
     expect(createMockTodoItemProps().handlers.onToggleDone).toHaveBeenCalledWith(createMockTodoItemProps().task.id, createMockTodoItemProps().task.isDone); //呼出チェック、h1とは異なりユーザーイベントが事前に必要
   });
   test("タスクをクリック：編集モードになる", async () => {
-   const displayElement = screen.getByLabelText('タスクのテキスト');
+   const displayElement = screen.getByLabelText(messages.TodoItem.textLabel);
    const user = userEvent.setup();
    await user.click(displayElement);
    expect(createMockTodoItemProps().handlers.onShowEdit).toHaveBeenCalledWith(createMockTodoItemProps().task.id); //呼出チェック、h1とは異なりユーザーイベントが事前に必要
@@ -45,10 +47,10 @@ describe("タスクコンポーネント", () => {
   test("タスクを編集：入力値が反映される", async () => {
     const user = userEvent.setup();
     const editableTask = {...createMockTodoItemProps().task, isEdit: true,};
-    await user.click(screen.getByLabelText('タスクのテキスト'));
+    await user.click(screen.getByLabelText(messages.TodoItem.textLabel));
     expect(createMockTodoItemProps().handlers.onShowEdit).toHaveBeenCalledWith(createMockTodoItemProps().task.id);
     rerender(<TodoItem {...createMockTodoItemProps()} task={editableTask} />); //型に応じたモックデータを渡す
-    const inputElement = screen.getByLabelText('タスク編集');
+    const inputElement = screen.getByLabelText(messages.TodoItem.editLabel);
     await user.clear(inputElement);
     await user.type(inputElement, 'updated');
     expect(createMockTodoItemProps().handlers.onChangeText).toHaveBeenCalledWith(createMockTodoItemProps().task.id, 'updated');
