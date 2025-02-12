8e0afc26e6af0c9bdb13427e2e7801ff4a595d22
mitchy-dev <mitchy.dev@gmail.com>
2025-02-12 16:40:25 +0900

    feat: APIクライアントを網羅

--- js-version/src/lib/todos.js
+++ js-version/src/lib/todos.js
@@ -7,3 +7,41 @@ export async function fetchTodos() {
   }
   return response.json();
 }
+
+export async function creteTodo(todo) {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  });
+  if (!response.ok) {
+    throw new Error('Failed to create todo')
+  }
+  return response.json();
+}
+
+export async function updateTodo(id, updates) {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updates),
+  });
+  if (!response.ok) {
+    throw new Error('Failed to update todo');
+  }
+  return response.json();
+}
+
+export async function deleteTodo(id) {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE',
+  });
+  if (!response.ok) {
+    throw new Error('Failed to delete todo');
+  }
+  return response.json();
+}
